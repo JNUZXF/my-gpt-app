dad5b3021d18c51be7f1b3778296cdda1e17358d
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-15 20:18:13 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,25 @@
+// components/ChatBox.js
 import React, { useState } from "react";
-import ChatBox from "../components/ChatBox";
-import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const ChatBox = ({ messages = [], onSendMessage }) => {
+  const [input, setInput] = useState('');
 
-const IndexPage = () => {
-  const [messages, setMessages] = useState([
-    { role: 'system', content: 'You are a helpful assistant.' },
-  ]);
-
-  const handleSendMessage = async (message) => {
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { role: 'user', content: message },
-    ]);
-
-    const completion = await openai.createChatCompletion({
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-          },
-        model: "gpt-3.5-turbo",
-      messages: [
-        ...messages,
-        { role: 'user', content: message },
-      ],
-    });
-
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { role: 'assistant', content: completion.data.choices[0].message.content },
-    ]);
+  const handleSend = () => {
+    if (input.trim() !== '') {
+      onSendMessage(input);
+      setInput('');
+    }
   };
 
   return (
-    <div className="container">
-      <h1 className="title">Welcome to our chat!</h1>
-      <ChatBox messages={messages} onSendMessage={handleSendMessage} />
+    <div className="chat-box">
+      {messages.map((message, index) => (
+        <p key={index}>{message.role === 'user' ? `You: ${message.content}` : `Assistant: ${message.content}`}</p>
+      ))}
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={handleSend}>Send</button>
     </div>
   );
 };
 
-export default IndexPage;
+export default ChatBox;
