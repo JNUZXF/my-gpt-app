77b69be83ae8b2d19726f64eba0ab843f54125b1
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-15 20:18:42 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,42 @@
-// components/ChatBox.js
+// pages/index.js
 import React, { useState } from "react";
+import ChatBox from "../components/ChatBox";
+import axios from 'axios';
 
-const ChatBox = ({ messages = [], onSendMessage }) => {
-  const [input, setInput] = useState('');
+const IndexPage = () => {
+  const [messages, setMessages] = useState([
+    { role: 'system', content: 'You are a helpful assistant.' },
+  ]);
 
-  const handleSend = () => {
-    if (input.trim() !== '') {
-      onSendMessage(input);
-      setInput('');
+  const handleSendMessage = async (message) => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { role: 'user', content: message },
+    ]);
+
+    try {
+      const response = await axios.post('/api/chat', {
+        messages: [
+          ...messages,
+          { role: 'user', content: message },
+        ],
+      });
+
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { role: 'assistant', content: response.data.choices[0].message.content },
+      ]);
+    } catch (error) {
+      console.error('Error calling chat API', error);
     }
   };
 
   return (
-    <div className="chat-box">
-      {messages.map((message, index) => (
-        <p key={index}>{message.role === 'user' ? `You: ${message.content}` : `Assistant: ${message.content}`}</p>
-      ))}
-      <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={handleSend}>Send</button>
+    <div className="container">
+      <h1 className="title">Welcome to our chat!</h1>
+      <ChatBox messages={messages} onSendMessage={handleSendMessage} />
     </div>
   );
 };
 
-export default ChatBox;
+export default IndexPage;
