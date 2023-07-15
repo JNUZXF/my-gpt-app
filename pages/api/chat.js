d6d87e324a11696bd364ddd161d40a0e45751434
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-15 21:41:38 +0800

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -10,11 +10,7 @@ export default async function handler(req, res) {
   const { messages } = req.body;
 
   try {
-    const result = await openai.createChatCompletion({
-    headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-          },    
+    const result = await openai.createChatCompletion({   
       model: "gpt-3.5-turbo",
       messages: messages,
     });
