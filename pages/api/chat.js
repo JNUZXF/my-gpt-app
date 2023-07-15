c2934f14d6a42a30c84f29a5ed2bdb5164c6075d
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-15 22:10:05 +0800

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-          },    
+          },  
       model: "gpt-3.5-turbo",
       messages: messages,
     });
