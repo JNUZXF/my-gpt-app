72aaa5d8ad135d728de13472c54739eea0bc2f9d
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-15 21:34:18 +0800

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,35 +1,24 @@
 // pages/api/chat.js
 import { Configuration, OpenAIApi } from "openai";
-import fetch from "node-fetch";
 
 export default async function handler(req, res) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
   const { messages } = req.body;
 
   try {
-    const fetchResponse = await fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-      },
-      body: JSON.stringify({
-        model: 'gpt-3.5-turbo',
-        messages: messages,
-        stream: true,  // Enable streaming
-      })
+    const result = await openai.createChatCompletion({
+    headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${key}`,
+          },    
+      model: "gpt-3.5-turbo",
+      messages: messages,
     });
-
-    if (!fetchResponse.ok) {
-      // Forward the error response
-      res.status(fetchResponse.status);
-      res.send(await fetchResponse.text());
-      return;
-    }
-
-    // Forward the response as a stream
-    res.setHeader('Content-Type', 'application/json');
-    fetchResponse.body.pipe(res);
-
+    res.status(200).json(result.data);
   } catch (error) {
     console.error('Error calling OpenAI API', error);
     res.status(500).json({ error: error.toString() });
